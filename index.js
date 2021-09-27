2b88924afb9ffa89586e705abee30ce69f32cb7a
faruk4234 <faruk.cetin4234@gmail.com>
2021-09-27 10:19:51 +0300

    discord

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {Client,Intents}= require('discord.js')
+const {Client,Intents }= require('discord.js')
 const {token} = require('./config.json')
 
 const client = new Client({intents:[Intents.FLAGS.GUILDS]})
@@ -7,5 +7,20 @@ client.once('ready',()=> {
   console.log('Ready!')
 })
 
-client.login(token)
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return
 
+  const { commandName } = interaction
+
+  if (commandName === 'ping') {
+    await interaction.reply('bada bumg')
+  } else if (commandName === 'server') {
+    await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`)
+  } else if (commandName === 'user') {
+    await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`)
+  }else if (commandName === 'farukbaba'){
+    await interaction.reply('that my fucking balls')
+  }
+})
+
+client.login(token)
